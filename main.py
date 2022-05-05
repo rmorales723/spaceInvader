aba8d88d9029e6d1eea1bda02697dad6ed767592
robert morales <rmoralfit@gmail.com>
2022-05-05 10:20:37 -0400

    added ship to bottom of game screen "yay"

--- main.py
+++ main.py
@@ -11,12 +11,25 @@ icon = pygame.image.load('space-invaders.png')
 pygame.display.set_icon(icon)
 
 
+# player
+playerImg = pygame.image.load('space-invaders.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 running = True
 while running:
+
+    # RGB- red, green, blue
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-# RGB- red, green, blue
-    screen.fill((0, 0, 0))
+    player()
     pygame.display.update()
+
+
