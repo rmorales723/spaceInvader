795844b1978394d4f95417e32c4fd5d7d14f9c58
robert morales <rmoralfit@gmail.com>
2022-05-05 15:37:11 -0400

    added bullet image and working on functioning

--- main.py
+++ main.py
@@ -30,11 +30,24 @@ enemyY = random.randint(50, 150)
 enemyX_change = 0.6
 enemyY_change = 40
 
+# bullet
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
 def player(x,y):
     screen.blit(playerImg, (x, y))
 
 def enemy(x,y):
     screen.blit(enemyImg, (x, y))
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
 
 # game loop
 running = True
@@ -54,9 +67,12 @@ while running:
                 playerX_change = -5
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.5
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
+
 
 # Checking for boundaries of spaceship so it doesn`t go out of bounds
 
@@ -76,6 +92,11 @@ while running:
     elif enemyX >= 736:
         enemyX_change = -3
         enemyY += enemyY_change
+
+# bullet movement
+    if bullet_state is "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
